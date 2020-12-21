cea3c230ae0474bd20742f6a76307a44ad754d0b
Blake Smith <blakesmith0@gmail.com>
2020-12-21 12:38:08 -0600

    Send a ping command to the micropad

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,28 +2,79 @@ use clap::{App, Arg, SubCommand};
 use log;
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
+use micropad_protocol::{Message, MessageFrame, Response};
+
+use serialport::{SerialPort, SerialPortInfo, SerialPortType};
+use std::time::Duration;
 
 #[derive(Debug)]
-enum IfError {
+enum CliError {
     Serial(serialport::Error),
+    Io(std::io::Error),
+    NotFound,
 }
 
-impl From<serialport::Error> for IfError {
-    fn from(err: serialport::Error) -> IfError {
-        IfError::Serial(err)
+impl From<serialport::Error> for CliError {
+    fn from(err: serialport::Error) -> CliError {
+        CliError::Serial(err)
     }
 }
 
-fn ping() -> Result<(), IfError> {
+impl From<std::io::Error> for CliError {
+    fn from(err: std::io::Error) -> CliError {
+        CliError::Io(err)
+    }
+}
+
+fn find_micropads() -> Result<Box<dyn Iterator<Item = SerialPortInfo>>, CliError> {
     let available_ports = serialport::available_ports()?;
-    log::info!("Got {} available serial ports", available_ports.len());
-    for port_info in available_ports {
-        log::info!(
-            "Got port: {}, type: {:?}",
-            port_info.port_name,
-            port_info.port_type
-        );
+    Ok(Box::new(available_ports.into_iter().filter(
+        |info| match info.port_type {
+            SerialPortType::UsbPort(ref usb_port_info) => match usb_port_info.serial_number {
+                Some(ref serial) if serial.starts_with("MP00") => true,
+                _ => false,
+            },
+            _ => false,
+        },
+    )))
+}
+
+fn find_micropad(position: usize) -> Result<SerialPortInfo, CliError> {
+    find_micropads()?
+        .nth(position)
+        .ok_or(CliError::NotFound)
+        .map_err(|err| match err {
+            CliError::NotFound => {
+                log::error!(
+                    "Could not find an attached micropad at position: {}, is it plugged in?",
+                    position
+                );
+                CliError::NotFound
+            }
+            err => err,
+        })
+}
+
+fn connect_micropad(port_info: &SerialPortInfo) -> Result<Box<dyn SerialPort>, CliError> {
+    serialport::new(&port_info.port_name, 11520)
+        .timeout(Duration::from_millis(500))
+        .open()
+        .map_err(|err| err.into())
+}
+
+fn ping() -> Result<(), CliError> {
+    let micropad_info = find_micropad(0)?;
+    let mut micropad_port = connect_micropad(&micropad_info)?;
+
+    let request_frame = MessageFrame::from(&Message::Ping);
+    micropad_port.write(&request_frame.buf)?;
+    let mut response_buf: [u8; 1] = [0x0; 1];
+    micropad_port.read(&mut response_buf)?;
+    match Response::from(response_buf[0]) {
+        Response::Ok => log::info!("Got ping response!"),
+        response => log::info!("Got non-ok response: {:?}", response)
     }
+
     Ok(())
 }
 
--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -14,11 +14,23 @@ pub enum Response {
     Ok = 0x00,
     LedDisabled = 0x01,
     UnknownMessage = 0x02,
+    Unknown = 0xFF,
 }
 
 impl Response {
     pub fn code(&self) -> u8 {
         *self as u8
+    }
+}
+
+impl From<u8> for Response {
+    fn from(code: u8) -> Response {
+        match code {
+            0x00 => Response::Ok,
+            0x01 => Response::LedDisabled,
+            0x02 => Response::UnknownMessage,
+            _ => Response::Unknown,
+        }
     }
 }
 
@@ -43,5 +55,25 @@ impl From<&MessageFrame> for Message {
             0x04 => Message::ChangeLed(frame.buf[1], frame.buf[2], frame.buf[3]),
             _ => Message::Unknown,
         }
+    }
+}
+
+impl From<&Message> for MessageFrame {
+    fn from(message: &Message) -> Self {
+        let mut message_frame = MessageFrame::new();
+        match message {
+            Message::Ping => {
+                message_frame.buf[0] = 0x01;
+                for i in 1..4 {
+                    message_frame.buf[i] = 0x00;
+                }
+            },
+            _ => {
+                for i in 0..4 {
+                    message_frame.buf[i] = 0x00;
+                }
+            }
+        }
+        message_frame
     }
 }
